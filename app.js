881f07b568028bfeccd447a6b50165e9aef93655
Lashon Hudson <lashonh74@gmail.com>
2020-12-11 21:15:07 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,124 @@
+// Global variables
+let controller;
+let slideScene;
+let pageScene;
+
+function animateSlides() {
+  // Init Controller
+  controller = new ScrollMagic.Controller();
+  // Select somethings
+  const sliders = document.querySelectorAll(".slide");
+  const nav = document.querySelector(".nav-header");
+  // Loop over each slider
+  sliders.forEach((slide, index, slides) => {
+    const revealImg = slide.querySelector(".reveal-img");
+    const img = slide.querySelector("img");
+    const revealText = slide.querySelector(".reveal-text");
+    /* GSAP must do gsap timeline gsap.to(revealImg, 1, { x: "100%" }); 
+    below is the anamation dafualt used the animation on the img and the img div that was covering it -=1 allows the animations to start at the same time*/
+    const slideT1 = gsap.timeline({
+      defaults: { duration: 1, ease: "power2.inOut" },
+    });
+    //   Gives the starting and end point of the animation
+    slideT1.fromTo(revealImg, { x: "0%" }, { x: "100%" });
+    slideT1.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
+    slideT1.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
+    slideT1.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
+    //   Create Scene loops over each slide and starts on the slides with the trigger element hooks repersent where the element is divided reverse false mean the animaion wont start again
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0.25,
+      reverse: false,
+    })
+      // Tween is used to start the timeline indicator marks where the trigger is
+      .setTween(slideT1)
+      .addIndicators({
+        colorStart: "white",
+        colorTrigger: "white",
+        name: "slide",
+      })
+      .addTo(controller);
+    //   New Animation
+    const pageT1 = gsap.timeline();
+    let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+    pageT1.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
+    pageT1.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+    pageT1.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
+
+    //   Create new scene durration lasts the whole height of the slide
+    pageScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0,
+    })
+      //Allows a section to stay in one place and pushfollwers gets rid of the space inbetween
+      .setPin(slide, { pushFollowers: false })
+      .setTween(pageT1)
+      .addIndicators({
+        colorStart: "white",
+        colorTrigger: "white",
+        name: "page",
+        indent: 200,
+      })
+      .addTo(controller);
+  });
+}
+const mouse = document.querySelector(".cursor");
+const mouseTxt = mouse.querySelector("span");
+const burger = document.querySelector(".burger");
+
+function cursor(e) {
+  mouse.style.top = e.pageY + "px";
+  mouse.style.left = e.pageX + "px";
+}
+// Giving the cursor animations functionality
+function activeCursor(e) {
+  const item = e.target;
+  if (item.id === "logo" || item.classList.contains("burger")) {
+    mouse.classList.add("nav-active");
+  } else {
+    mouse.classList.remove("nav-active");
+  }
+  if (item.classList.contains("explore")) {
+    mouse.classList.add("explore-active");
+    //   Fills in the title with the color
+    gsap.to(".title-swipe", 1, { y: "0%" });
+    mouseTxt.innerText = "Tap";
+  } else {
+    mouse.classList.remove("explore-active");
+    mouseTxt.innerText = "";
+    gsap.to(".title-swipe", 1, { y: "100%" });
+  }
+}
+function navToggle(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%" });
+    document.body.classList.add("hide");
+  } else {
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to("#logo", 1, { color: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%" });
+    document.body.classList.remove("hide");
+  }
+}
+
+// Barba Page Transitions
+barba.init({
+    views: [
+        {
+            namespace:"home"
+        }
+    ]
+})
+
+// Adding eventlistners to cursor element using the window methods
+window.addEventListener("mousemove", cursor);
+window.addEventListener("mousemove", activeCursor);
+burger.addEventListener("click", navToggle);
+animateSlides();
